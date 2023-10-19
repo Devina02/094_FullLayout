d4e9be4dddfc86e67d121f81cc51576d2ee7f349
Devina02 <devina.fitriapngs@gmail.com>
2023-10-19 12:05:55 +0700

    first

--- PAM/PAM2/app/src/main/java/com/example/pam2/activity_linear_layout.kt
+++ PAM/PAM2/app/src/main/java/com/example/pam2/activity_linear_layout.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class activity_linear_layout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_layout)
+        setContentView(R.layout.activity_main)
     }
 }
--- PAM/PAM2/app/src/main/java/com/example/pam2/activity_linear_layout2.kt
+++ PAM/PAM2/app/src/main/java/com/example/pam2/activity_linear_layout2.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class activity_linear_layout2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_layout2)
+        setContentView(R.layout.activity_linear_layout)
     }
 }
